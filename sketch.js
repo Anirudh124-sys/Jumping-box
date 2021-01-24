4426c4692b8f1dd43b911498d187c12171d1ec40
Anirudh124-sys <76613757+Anirudh124-sys@users.noreply.github.com>
2021-01-24 19:52:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,54 @@
+var canvas;
+var music;
+var surface1,surface2,surface3,surface4;
+var ball
+
+function preload(){
+    music = loadSound("music.mp3");
+}
+
+
+function setup(){
+    canvas = createCanvas(800,600);
+
+    //create 4 different surfaces
+     surface1 = createSprite(100,500,200,30)
+     surface1.shapeColor = "red"
+     surface2 = createSprite(300,500,200,30)
+     surface2.shapeColor = "blue"
+     surface3 = createSprite(500,500,200,30)
+     surface3.shapeColor = "green"
+     surface4 = createSprite(700,500,200,30)
+     surface4.shapeColor = "yellow"
+
+    //create box sprite and give velocity
+     ball = createSprite(random(20,750),200,20,20)
+     ball.shapecolor = "black"
+     ball.velocityX = 5
+     ball.velocityY = 5
+}
+
+function draw() {
+    background(rgb(169,169,169));
+    //create edgeSprite
+    edges=createEdgeSprites()
+    ball.bounceOff(edges)
+    if(surface1.isTouching(ball)&&ball.bounceOff(surface1)){
+        ball.shapeColor = "red"
+    }
+    if(surface2.isTouching(ball)&&ball.bounceOff(surface2)){
+        ball.shapeColor = "blue"
+        ball.velocityX = 0
+        ball.velocityY =0
+    }
+    if(surface3.isTouching(ball)&&ball.bounceOff(surface3)){
+        ball.shapeColor = "green"
+    }
+    if(surface4.isTouching(ball)&&ball.bounceOff(surface4)){
+        ball.shapeColor = "yellow"
+    }
+
+
+    //add condition to check if box touching surface and make it box
+drawSprites();
+}
